320b7ac52703979d7a3d2222289a7493c149a047
ShashankJaitly <shashank16vasu@gmail.com>
2020-05-27 00:48:34 +0530

    --update add python script to send masss mail using send grid

---
+++ python-masss-mail/mass_mail_using_sendgrid.py
@@ -0,0 +1,50 @@
+import os
+import json
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail, To
+
+# data = [
+#     {
+#     "email": "shashank16jaitly@gmail.com",
+#     "name": "Shashank Jaitly"
+#   },
+#   {                                                   Sample data
+#     "email": "shashank16vasu@gmail.com",
+#     "name": "Shashank"
+#   },
+# ]
+recipent_list=[]
+user_list = []
+for user in data:
+        recipent_list.append(user['email'])
+        user_list.append(user['name'])
+
+to_emails = []
+for recipent in range(0,len(recipent_list)):
+    email=To(email=recipent_list[recipent],
+            name=user_list[recipent],
+            substitutions={
+                '-name-': user_list[recipent],
+            })
+    to_emails.append(email)
+
+   
+
+message = Mail(
+    from_email=('shashank.1822co1053@kiet.edu', 'Shashank'),
+    to_emails=to_emails,
+    subject='Hi -name-, this is a test mail',
+    html_content="""\ place your html here """,
+    
+    is_multiple=True)
+try:
+    sendgrid_client = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    response = sendgrid_client.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e)
+
+
+
